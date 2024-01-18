a2f54de02d4aeb9b1494c086bcdd30faefec3ca3
Will James <willj@dealcloud.com>
2024-01-18 16:37:49 +0000

    add config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,32 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event="VeryLazy",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts ={
+      ensure_installed={
+        "eslint-lsp",
+        "prettier",
+        "typescript-language-server",
+        "gopls",
+        "pyright",
+        "mypy",
+        "ruff",
+        "black",
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+       require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
