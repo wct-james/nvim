865abe2bf4adab64565b2a68703a967da9bd39a6
Will James <willj@dealcloud.com>
2024-01-18 18:33:13 +0000

    Remove mypy

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -7,7 +7,6 @@ local opts = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports,
-    null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
     null_ls.builtins.formatting.black,
   },
